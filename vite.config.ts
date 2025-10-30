5972886e6b97eb03045304e176015e18ec8e26c1
DomeLandGHG <153361792+DomeLandGHG@users.noreply.github.com>
2025-10-30 19:22:58 +0100

    third_try

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/React-Dome/' : '/',
+  base: '/React-Dome/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
